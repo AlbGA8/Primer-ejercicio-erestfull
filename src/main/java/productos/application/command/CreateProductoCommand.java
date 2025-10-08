77d63399cac5f9cd24f3a9dcf69e3f95158d82c3
alba <alba@insti.com>
2025-10-08 11:19:50 -0500

    exito

--- src/main/java/productos/application/command/CreateProductoCommand.java
+++ src/main/java/productos/application/command/CreateProductoCommand.java
@@ -1,5 +1,17 @@
 package productos.application.command;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@AllArgsConstructor
+@Accessors(fluent = true)
+
 public class CreateProductoCommand {
+
+
+	private String nombre;
+	private double precio;
 
 }
